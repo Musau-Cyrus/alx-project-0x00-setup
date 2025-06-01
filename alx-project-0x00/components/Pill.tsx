64e4f5d4735098779239eb89208b4e4270136a3f
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-06-01 13:01:38 +0300

    An upate to correct some errors

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,10 +1,6 @@
-import React from "react"
+import { PillProps } from "@/interfaces";
 
-interface PillProps {
-    title: string;
-}
-
-const Pill: React.FC<PillProps> = ({ title }) => {
+const Pill: React.FC<PillProps> = ({title}) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
       <p className=" text-sm ">Title</p>
